72b5160d4c5cd1044cc5bd6e4fd3f735af44a66e
Kenneth Bryan Chung <kbchung18@gmail.com>
2016-03-02 19:32:44 +0800

    change some configs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => "public_profile, user_photos, pages_manage_cta, manage_pages"}
+  provider :facebook, ENV["fb_app_id"], ENV["fb_secret"], {:scope => "public_profile, user_photos, pages_manage_cta, manage_pages"}
 end
