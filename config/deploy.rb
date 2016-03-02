ef69c53d66271b4e27e55ca1892d56b5925d3cee
Kenneth Bryan Chung <kbchung18@gmail.com>
2016-03-02 23:45:35 +0800

    prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repo_url, 'git@github.com:kbc12345/paparazziph.git' # Edit this to match yo
 set :branch, :master
 set :deploy_to, '/home/deploy/paparazziph'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
@@ -25,3 +25,45 @@ set :puma_workers, 0
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
+
+# namespace :deploy do
+#   after :publishing, :restart
+# end
+
+task :install_bower do
+  on roles(:all) do
+    execute "cd #{release_path} && bower install"
+  end
+end
+before 'deploy:compile_assets', 'install_bower'
+after 'deploy', 'puma:start'
+
+# desc 'Initial Deploy'
+#   task :initial do
+#     on roles(:app) do
+#       before 'deploy:restart', 'puma:start'
+#       invoke 'deploy'
+#     end
+#   end
+
+#   desc 'Restart application'
+#   task :restart do
+#     on roles(:app), in: :sequence, wait: 5 do
+#       invoke 'puma:smart_restart'
+#     end
+#   end
+
+#   # task :custom_puma_restart do
+#   #   on roles (fetch(:puma_role)) do |role|
+#   #     within current_path do
+#   #       puma_switch_user(role) do
+#   #         execute "xargs -n 1 kill -s SIGUSR2 < #{shared_path}/tmp/pids/puma.pid"
+#   #       end
+#   #     end
+#   #   end
+#   # end
+
+#   before :starting,     :check_revision
+#   after  :finishing,    :compile_assets
+#   after  :finishing,    :cleanup
+#   after  :finishing,    :restart
