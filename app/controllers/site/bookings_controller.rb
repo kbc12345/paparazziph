17d447729e316701c0740d5d42c412f6ff692636
Kenneth Bryan Chung <kbchung18@gmail.com>
2016-03-12 20:56:32 +0800

    auto reply for bookings

--- app/controllers/site/bookings_controller.rb
+++ app/controllers/site/bookings_controller.rb
@@ -32,6 +32,34 @@ class Site::BookingsController < SiteController
     smtp.send_message mail.to_s(), 'thepaparazziteam@gmail.com', 'thepaparazziteam@gmail.com'
     smtp.finish
 
+    message = <<-EOF
+      Thank you for your interest in our service, we will contact you as soon as we possible to finalize your booking.
+      
+      In the mean time, our proposal is attached in this email which contains some info/terms of our service.
+
+      Should you have any questions, you may contact us @ 09778055808 for faster response.
+
+      Regards,
+      ThePaparazziTeam
+    EOF
+
+    mail = MailFactory.new()
+    mail.to = "#{params[:booking]["client_email"]}"
+    mail.from = "no-reply@paparazziph.com"
+    mail.subject = "Booking Request Received"
+    mail.text = message
+    if params[:booking]["category"] == "Photoman"
+      mail.attach('app/assets/files/photoman-proposal.docx')
+    else
+      mail.attach('app/assets/files/photo-coverage.docx')
+    end
+
+    smtp = Net::SMTP.new 'smtp.sparkpostmail.com', 587
+    smtp.enable_starttls
+    smtp.start('sparkpostmail.com', ENV["sparkpost_username"], ENV["sparkpost_password"], :login)
+    smtp.send_message mail.to_s(), 'no-reply@paparazziph.com', params[:booking]["client_email"]
+    smtp.finish
+
     render json: {}
   end
 end
